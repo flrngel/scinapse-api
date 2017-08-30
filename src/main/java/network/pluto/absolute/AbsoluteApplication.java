5fc11dbfd3a79d0529d4409c1a344f58dc983bab
am2rican5 <am2rican5@gmail.com>
2017-08-30 17:32:50 +0900

    Initial commit

---
+++ src/main/java/network/pluto/absolute/AbsoluteApplication.java
@@ -0,0 +1,12 @@
+package network.pluto.absolute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsoluteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbsoluteApplication.class, args);
+	}
+}
