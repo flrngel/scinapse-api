288280cadc21b4ba34dbf6da70e431eb9b35192a
Lee Hyeon <hyeon@pluto.network>
2017-10-23 14:45:07 +0900

    HTTP/2.0 support (#28)

--- src/main/java/network/pluto/absolute/AbsoluteApplication.java
+++ src/main/java/network/pluto/absolute/AbsoluteApplication.java
@@ -1,15 +1,18 @@
 package network.pluto.absolute;
 
+import io.undertow.UndertowOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.embedded.undertow.UndertowEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan({ "network.pluto.absolute", "network.pluto.bibliotheca" })
+@ComponentScan({"network.pluto.absolute", "network.pluto.bibliotheca"})
 @EnableJpaAuditing
 @EnableJpaRepositories("network.pluto.bibliotheca.repositories")
 @EntityScan(
@@ -19,5 +22,12 @@ public class AbsoluteApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AbsoluteApplication.class, args);
+    }
+
+    @Bean
+    public UndertowEmbeddedServletContainerFactory undertowEmbeddedServletContainerFactory() {
+        UndertowEmbeddedServletContainerFactory factory = new UndertowEmbeddedServletContainerFactory();
+        factory.addBuilderCustomizers(builder -> builder.setServerOption(UndertowOptions.ENABLE_HTTP2, true));
+        return factory;
     }
 }
