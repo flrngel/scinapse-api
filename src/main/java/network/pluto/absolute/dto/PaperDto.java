069adcb85c9befcc87021c6fe7f6da1865ebb628
tedyeee <tedyeee@gmail.com>
2017-12-05 11:38:38 +0900

    Rename paperAbstract field to abstract

--- src/main/java/network/pluto/absolute/dto/PaperDto.java
+++ src/main/java/network/pluto/absolute/dto/PaperDto.java
@@ -1,5 +1,6 @@
 package network.pluto.absolute.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -23,6 +24,7 @@ public class PaperDto {
 
     private Integer citation;
 
+    @JsonProperty("abstract")
     private String paperAbstract;
 
     private String lang;
