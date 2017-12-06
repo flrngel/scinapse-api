1157a371119887081231fed39c235cc08e63c5f3
tedyeee <tedyeee@gmail.com>
2017-12-06 13:42:50 +0900

    Modify paper related uris

--- src/main/java/network/pluto/absolute/controller/PaperController.java
+++ src/main/java/network/pluto/absolute/controller/PaperController.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.access.AuthorizationServiceException;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
@@ -29,7 +30,10 @@ public class PaperController {
     private final MemberService memberService;
 
     @Autowired
-    public PaperController(PaperService paperService, PaperFacade paperFacade, CommentService commentService, MemberService memberService) {
+    public PaperController(PaperService paperService,
+                           PaperFacade paperFacade,
+                           CommentService commentService,
+                           MemberService memberService) {
         this.paperService = paperService;
         this.paperFacade = paperFacade;
         this.commentService = commentService;
@@ -42,8 +46,13 @@ public class PaperController {
     }
 
     @RequestMapping(value = "/papers/search", method = RequestMethod.GET)
-    public Page<PaperDto> search(@RequestParam String text, @PageableDefault Pageable pageable) {
+    public Page<PaperDto> searchDeprecated(@RequestParam String text, @PageableDefault Pageable pageable) {
         return paperFacade.search(text, pageable);
+    }
+
+    @RequestMapping(value = "/papers", method = RequestMethod.GET)
+    public Page<PaperDto> search(@RequestParam String query, @PageableDefault Pageable pageable) {
+        return paperFacade.search(query, pageable);
     }
 
     @RequestMapping(value = "/papers/{paperId}/comments", method = RequestMethod.POST)
@@ -73,5 +82,38 @@ public class PaperController {
         }
 
         return commentService.findByPaper(paper, pageable).map(CommentDto::new);
+    }
+
+    @RequestMapping(value = "/papers/{paperId}/comments/{commentId}", method = RequestMethod.PUT)
+    public CommentDto updateComment(@ApiIgnore JwtUser user,
+                                    @PathVariable long commentId,
+                                    @RequestBody CommentDto commentDto) {
+        Comment comment = commentService.find(commentId);
+        if (comment == null) {
+            throw new ResourceNotFoundException("Comment not found");
+        }
+
+        if (comment.getCreatedBy().getId() != user.getId()) {
+            throw new AuthorizationServiceException("Members can update their own comment only");
+        }
+
+        Comment updated = commentService.updateComment(comment, commentDto.toEntity());
+        return new CommentDto(updated);
+    }
+
+    @RequestMapping(value = "/papers/{paperId}/comments/{commentId}", method = RequestMethod.DELETE)
+    public Result deleteComment(@ApiIgnore JwtUser user,
+                                @PathVariable long commentId) {
+        Comment comment = commentService.find(commentId);
+        if (comment == null) {
+            throw new ResourceNotFoundException("Comment not found");
+        }
+
+        if (comment.getCreatedBy().getId() != user.getId()) {
+            throw new AuthorizationServiceException("Deleting comment is only possible by its creator");
+        }
+
+        commentService.deleteComment(comment);
+        return Result.success();
     }
 }
