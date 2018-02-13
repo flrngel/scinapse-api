15dc6763cdcae8f4e842701eb32af7380f938e3a
tedyeee <tedyeee@gmail.com>
2018-02-13 18:15:45 +0900

    Send Sentry meaningful message for external api error (#62)

--- src/main/java/network/pluto/absolute/error/SentryExceptionResolver.java
+++ src/main/java/network/pluto/absolute/error/SentryExceptionResolver.java
@@ -4,6 +4,10 @@ import io.sentry.Sentry;
 import org.springframework.core.Ordered;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -23,6 +27,14 @@ public class SentryExceptionResolver implements HandlerExceptionResolver, Ordere
                 ex instanceof AuthenticationException) {
 
             // will not send exception event to sentry
+            return null;
+        }
+
+        // Below exception's getMessage() is not much helpful for debug. Converting to meaningful message.
+        if (ex instanceof HttpClientErrorException || ex instanceof HttpServerErrorException) {
+            HttpStatusCodeException restEx = (HttpStatusCodeException) ex;
+            RestClientException convertedEx = new RestClientException(restEx.getResponseBodyAsString(), ex);
+            Sentry.capture(convertedEx);
             return null;
         }
 
