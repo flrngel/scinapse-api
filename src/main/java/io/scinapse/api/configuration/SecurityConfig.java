da347b25137f7df8f371693f9e165f085f1e494b
tedyeee <tedyeee@gmail.com>
2018-08-14 17:31:40 +0900

    Add security exception for journal API (#118)

--- src/main/java/io/scinapse/api/configuration/SecurityConfig.java
+++ src/main/java/io/scinapse/api/configuration/SecurityConfig.java
@@ -115,6 +115,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/authors/*",
                         "/authors/*/papers",
                         "/authors/*/co-authors",
+                        "/journals/*",
+                        "/journals/*/papers",
                         "/collections/*",
                         "/collections/*/papers",
                         "/comments",
