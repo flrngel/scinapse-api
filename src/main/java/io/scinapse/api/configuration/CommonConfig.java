d9619b1957294aa20502b17b2103721560d3c0ec
tedyeee <tedyeee@gmail.com>
2018-09-17 19:48:09 +0900

    Prevent Invalid 'expires' cookie WARN log (#131)
    * Do not use external properties for local profile
    
    * Prevent "Invalid 'expires' attribute" WARN log
    
    * Replace cookie spec registry of RestTemplate

--- src/main/java/io/scinapse/api/configuration/CommonConfig.java
+++ src/main/java/io/scinapse/api/configuration/CommonConfig.java
@@ -2,6 +2,15 @@ package io.scinapse.api.configuration;
 
 import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter;
 import org.apache.http.HttpHost;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.utils.DateUtils;
+import org.apache.http.config.Lookup;
+import org.apache.http.conn.util.PublicSuffixMatcherLoader;
+import org.apache.http.cookie.CookieSpecProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.CookieSpecRegistries;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.cookie.DefaultCookieSpecProvider;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -9,6 +18,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -27,7 +37,13 @@ public class CommonConfig {
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        CloseableHttpClient client = HttpClients.custom()
+                .setDefaultCookieSpecRegistry(createCookieSpecRegistry())
+                .build();
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(client);
+
         return builder
+                .requestFactory(factory)
                 .additionalMessageConverters(new FormHttpMessageConverter())
                 .additionalMessageConverters(new StringHttpMessageConverter(Charset.forName("UTF-8")))
                 .setConnectTimeout(2000)
@@ -39,13 +55,38 @@ public class CommonConfig {
     public RestHighLevelClient restHighLevelClient(@Value("${pluto.server.es.hostname}") String hostname,
                                                    @Value("${pluto.server.es.port}") int port,
                                                    @Value("${pluto.server.es.scheme}") String scheme) {
-
         RestClientBuilder builder = RestClient
                 .builder(new HttpHost(hostname, port, scheme))
+
                 // we cannot use bug fixed client(6.3.1+) due to ES version conflict(6.2)
-                .setRequestConfigCallback(requestConfigBuilder -> requestConfigBuilder.setConnectionRequestTimeout(0));
+                .setRequestConfigCallback(config -> config.setConnectionRequestTimeout(0))
+
+                // prevent "Invalid 'expires' attribute" WARN log
+                .setHttpClientConfigCallback(config -> config.setDefaultCookieSpecRegistry(createCookieSpecRegistry()));
 
         return new RestHighLevelClient(builder);
+    }
+
+    private Lookup<CookieSpecProvider> createCookieSpecRegistry() {
+        String[] datePatterns = {
+                "EEE, dd-MMM-yy HH:mm:ss z", // from NetscapeDraftSpec.EXPIRES_PATTERN
+                DateUtils.PATTERN_RFC1123, // from RFC6265StrictSpec.DATE_PATTERNS
+                DateUtils.PATTERN_RFC1036,
+                DateUtils.PATTERN_ASCTIME
+        };
+
+        // default cookie spec does not have standard 'expires' date patterns
+        // we need to override default date pattern
+        CookieSpecProvider defaultProvider = new DefaultCookieSpecProvider(
+                DefaultCookieSpecProvider.CompatibilityLevel.DEFAULT,
+                PublicSuffixMatcherLoader.getDefault(),
+                datePatterns,
+                false);
+
+        // only override default cookie spec
+        return CookieSpecRegistries.createDefaultBuilder()
+                .register(CookieSpecs.DEFAULT, defaultProvider)
+                .build();
     }
 
     @Bean
