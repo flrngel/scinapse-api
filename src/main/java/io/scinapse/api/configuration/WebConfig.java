00be401637f0195a210266277d5efe2dc8e7cee2
roamcne <roamcne@gmail.com>
2019-01-24 17:43:50 +0900

    Set default content type (#192)
    - ignore accept header

--- src/main/java/io/scinapse/api/configuration/WebConfig.java
+++ src/main/java/io/scinapse/api/configuration/WebConfig.java
@@ -2,7 +2,9 @@ package io.scinapse.api.configuration;
 
 import io.scinapse.api.controller.PageRequestResolver;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.List;
@@ -13,6 +15,11 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(new PageRequestResolver());
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.ignoreAcceptHeader(true).defaultContentType(MediaType.APPLICATION_JSON);
     }
 
 }
